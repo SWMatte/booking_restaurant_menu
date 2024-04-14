79e08f860259b9e5e5cbce684abb20d72d8e2ed6
SWMatte <peirettimatteo1@gmail.com>
2024-04-14 11:41:12 +0200

    add swagger API

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Restaurant online
 
-here i'll write the documentation about this project, actually there are some explanation about methods in the folder 
-[documentazione](doc/index.html)
+here i'll write the documentation about this project, actually there are some explanation about methods in the follower link
+http://localhost:8080/api/v1/swagger-ui/index.html .
+
+Soon i'll write more documentation about every single field in this project
+but basically a brief summary could be: " Application to book some specific products and show to the caller how much will spend and which products will be buy on the pdf document"
+
+- maybe in the future i'll develop a front-end application in according to these end-point
+- need to develop more features like PDF documentation, several fixes
--- src/main/java/restaurant/menu/MenuApplication.java
+++ src/main/java/restaurant/menu/MenuApplication.java
@@ -3,15 +3,22 @@ package restaurant.menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
 })
-public class MenuApplication {
+public class MenuApplication  implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MenuApplication.class, args);
 	}
-
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/swagger-ui/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/springdoc-openapi-ui/")
+				.resourceChain(false);
+	}
 }
